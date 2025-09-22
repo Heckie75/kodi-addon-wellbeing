ddb7adfb91d9607fc846f87221f783aa22c918ff
Heckie75 <github@heckie.de>
2025-09-22 09:37:33 +0200

    [script.wellbeing] 1.3.1

--- script.wellbeing/script.py
+++ script.wellbeing/script.py
@@ -1,7 +1,10 @@
-import xbmcaddon
-import xbmcgui
+import re
 import sys
 import time
+
+import xbmc
+import xbmcaddon
+import xbmcgui
 
 addon = xbmcaddon.Addon()
 
@@ -20,10 +23,17 @@ if __name__ == "__main__":
         time.sleep(1)  # wait for settings to be saved
         addon.setSettingInt("sum", -1)
 
-    elif len(sys.argv) > 1 and sys.argv[1] == "action=limit":
+    elif len(sys.argv) > 1 and sys.argv[1] == "action=limit" and sys.args[2].startswith("limit="):
 
         time_ = sys.argv[2][6:]
-        addon.setSetting("limit", time_)
+        if re.match(r"^\d{1,2}:\d{2}$", time_):
+            xbmc.log(f"script.wellbeing: setting time limit to '{time_}'", xbmc.LOGINFO)
+            addon.setSetting("limit", time_)
+        elif time_.isdigit():
+            xbmc.log(f"script.wellbeing: converting time '{time_}' to mm:ss", xbmc.LOGINFO)
+            addon.setSetting("limit", f"{(int(time_) // 60):02d}:{(int(time_) % 60):02d}")
+        else:
+            xbmc.log(f"script.wellbeing: invalid time format '{time_}'", xbmc.LOGERROR)
 
     elif len(sys.argv) > 1 and sys.argv[1] == "limit":
 
